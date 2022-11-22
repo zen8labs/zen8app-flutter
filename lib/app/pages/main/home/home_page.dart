643b3bbf6ceab1339b3cb3baa0e2271cdd527095
toannt <toannt.ce@gmail.com>
2022-11-22 17:30:26 +0700

    test basic mvvm pattern

--- lib/app/pages/main/home/home_page.dart
+++ lib/app/pages/main/home/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:zen8app/app/pages/main/home/home_vm.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,10 +11,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _homeVM = HomeViewModel();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: Text("Home"),
+        actions: [],
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+              StreamBuilder(
+                stream: _homeVM.outToken,
+                builder: (context, snapshot) {
+                  var text = snapshot.data?.toString() ?? "!!!!";
+                  return Text(text);
+                },
+              ),
+              ElevatedButton(
+                child: Text("Test"),
+                onPressed: () async {
+                  _homeVM.inLogin.add(null);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
